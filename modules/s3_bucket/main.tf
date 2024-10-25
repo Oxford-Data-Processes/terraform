655d35d40305a93a375559dc73cba80806fcbd16
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-10-25 16:53:50 +0100

    Enable versioning

--- modules/s3_bucket/main.tf
+++ modules/s3_bucket/main.tf
@@ -1,8 +1,11 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
 
