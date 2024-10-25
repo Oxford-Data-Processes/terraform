cb14d1c20f2b92fdad4df6ea7f0dabcd2c52231c
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-10-25 17:04:12 +0100

    Disable publick access block

--- modules/s3_bucket/main.tf
+++ modules/s3_bucket/main.tf
@@ -2,6 +2,15 @@ resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = false
+  ignore_public_acls      = false
+  block_public_policy     = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_versioning" "s3_bucket_versioning" {
   bucket = aws_s3_bucket.s3_bucket.id
   versioning_configuration {
@@ -13,3 +22,4 @@ resource "aws_s3_bucket_policy" "s3_bucket_policy" {
   bucket = aws_s3_bucket.s3_bucket.bucket
   policy = data.aws_iam_policy_document.bucket_policy.json
 }
+
