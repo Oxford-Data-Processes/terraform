d84bc6ab5d0c2a7140dd17210367abfd2194b945
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-10-25 16:37:18 +0100

    Add general permissions

--- modules/s3_bucket/data.tf
+++ modules/s3_bucket/data.tf
@@ -1,17 +1,13 @@
 data "aws_iam_policy_document" "bucket_policy" {
   statement {
-    principals {
-      type        = "AWS"
-      identifiers = [aws_iam_role.this.arn]
-    }
-
-    actions = [
-      "s3:ListBucket", "s3:GetObject", "s3:PutObject"
-    ]
-
+    actions = ["s3:GetObject", "s3:PutObject", "s3:ListBucket", "s3:GetBucketLocation"]
     resources = [
       "arn:aws:s3:::${var.bucket_name}",
       "arn:aws:s3:::${var.bucket_name}/*"
     ]
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
   }
 }
