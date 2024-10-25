795d7481575ce7c58d1559d18582da027f5762e1
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-10-25 16:43:00 +0100

    Made pretty

--- modules/s3_bucket/main.tf
+++ modules/s3_bucket/main.tf
@@ -1,12 +1,10 @@
-
 module "s3_bucket_module" {
-  source  = "terraform-aws-modules/s3-bucket/aws"
-  bucket  = var.bucket_name
-  acl     = "private"
+  source        = "terraform-aws-modules/s3-bucket/aws"
+  bucket        = var.bucket_name
   attach_policy = true
-  policy  = data.aws_iam_policy_document.bucket_policy.json
+  policy        = data.aws_iam_policy_document.bucket_policy.json
 
-  versioning = {
+  versioning {
     enabled = true
   }
 }
