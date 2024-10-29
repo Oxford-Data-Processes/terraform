11fd75bd7f8a03c160dfb7a418fe1a82968b16ff
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-10-29 14:43:40 +0000

    add eventbridge

--- modules/eventbridge/main.tf
+++ modules/eventbridge/main.tf
@@ -1,31 +1,25 @@
+resource "aws_cloudwatch_event_bus" "event_bus" {
+  name = "${var.project}-${var.lambda_function_name}-event-bus"
+}
+
+resource "aws_cloudwatch_event_rule" "event_bus_rule" {
+  name               = "${var.project}-${var.lambda_function_name}-event-bus-rule"
+  event_bus_name     = aws_cloudwatch_event_bus.event_bus.name
+  event_pattern      = jsonencode({ "source" : ["com.oxforddataprocesses"] })
+  state              = "ENABLED"
+}
 
 resource "aws_lambda_permission" "allow_eventbridge" {
   statement_id  = "AllowExecutionFromEventBridge"
   action        = "lambda:InvokeFunction"
-  function_name = var.lambda_function_name
+  function_name = "${var.project}-${var.lambda_function_name}"
   principal     = "events.amazonaws.com"
-  source_arn    = "arn:aws:events:${var.aws_region}:${var.aws_account_id}:rule/${local.rule_name}"
+  source_arn    = aws_cloudwatch_event_rule.event_bus_rule.arn
 }
 
-module "eventbridge" {
-  source = "terraform-aws-modules/eventbridge/aws"
-
-  bus_name = "${var.project}-${var.lambda_function_name}-event-bus"
-
-  rules = {
-    global_event_bus = {
-      name          = local.rule_name
-      event_pattern = jsonencode({ "source": ["com.oxforddataprocesses"] })
-      enabled       = true
-    }
-  }
-
-  targets = {
-    global_event_bus = [
-      {
-        name = local.target_name
-        arn  = "arn:aws:lambda:${var.aws_region}:${var.aws_account_id}:function:${local.lambda_function}"
-      },
-    ]
-  }
+resource "aws_cloudwatch_event_target" "event_bus_target" {
+  rule          = aws_cloudwatch_event_rule.event_bus_rule.name
+  event_bus_name = aws_cloudwatch_event_bus.event_bus.name
+  arn           = "arn:aws:lambda:${var.aws_region}:${var.aws_account_id}:function:${var.project}-${var.lambda_function_name}"
+  target_id     = "${var.project}-${var.lambda_function_name}-target"
 }
---
+++ modules/eventbridge/variables.tf
@@ -0,0 +1,20 @@
+variable "lambda_function_name" {
+  description = "The name of the Lambda function"
+  type        = string
+}
+
+variable "aws_account_id" {
+  description = "The AWS account ID"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "The AWS region where the Lambda function is deployed"
+  type        = string
+}
+
+variable "project" {
+  description = "The name of the project"
+  type        = string
+}
+
