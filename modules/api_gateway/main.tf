c604449a8f5401069e3620ce78ec41e718c88f08
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-11-07 10:45:06 +0000

    Add put request

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -26,7 +26,7 @@ resource "aws_api_gateway_rest_api" "rest_api" {
 resource "aws_api_gateway_resource" "api_resource" {
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-  path_part   = var.api_resource_path_part
+  path_part   = "items"
 }
 
 resource "aws_api_gateway_method" "api_method" {
@@ -40,6 +40,23 @@ resource "aws_api_gateway_integration" "api_integration" {
   rest_api_id             = aws_api_gateway_rest_api.rest_api.id
   resource_id             = aws_api_gateway_resource.api_resource.id
   http_method             = aws_api_gateway_method.api_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+
+  uri = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${local.api_lambda_arn}/invocations"
+}
+
+resource "aws_api_gateway_method" "api_method_put" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  resource_id   = aws_api_gateway_resource.api_resource.id
+  http_method   = "PUT"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "api_integration_put" {
+  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
+  resource_id             = aws_api_gateway_resource.api_resource.id
+  http_method             = aws_api_gateway_method.api_method_put.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
 
