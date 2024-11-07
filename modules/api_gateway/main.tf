09b1c156f5fb99ef8127d10c7ac327ee9b9690e1
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-11-07 10:48:15 +0000

    Fix api

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -1,86 +1,10 @@
-locals {
-  lambda_function_name = "${var.project}-api-lambda"
-  api_lambda_arn       = "arn:aws:lambda:${var.aws_region}:${var.aws_account_id}:function:${var.project}-api-lambda"
+module "api_gateway" {
+  source                      = "git::https://github.com/Oxford-Data-Processes/terraform.git//modules/api_gateway"
+  aws_account_id              = var.aws_account_id
+  aws_region                  = var.aws_region
+  stage                       = var.stage
+  project                     = var.project
+  aws_access_key_id_admin     = var.aws_access_key_id_admin
+  aws_secret_access_key_admin = var.aws_secret_access_key_admin
+  version_number              = var.version_number
 }
-
-module "api_gateway_lambda" {
-  source         = "terraform-aws-modules/lambda/aws"
-  function_name  = local.lambda_function_name
-  create_package = false
-  image_uri      = "${var.aws_account_id}.dkr.ecr.${var.aws_region}.amazonaws.com/${local.lambda_function_name}-image:${var.version_number}"
-  package_type   = "Image"
-
-  environment_variables = {
-    STAGE                       = var.stage
-    AWS_ACCESS_KEY_ID_ADMIN     = var.aws_access_key_id_admin
-    AWS_SECRET_ACCESS_KEY_ADMIN = var.aws_secret_access_key_admin
-  }
-  timeout     = 900
-  memory_size = 2048
-}
-
-resource "aws_api_gateway_rest_api" "rest_api" {
-  name = "${var.project}-api"
-}
-
-resource "aws_api_gateway_resource" "api_resource" {
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-  path_part   = "items"
-}
-
-resource "aws_api_gateway_method" "api_method" {
-  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-  resource_id   = aws_api_gateway_resource.api_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "api_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
-  resource_id             = aws_api_gateway_resource.api_resource.id
-  http_method             = aws_api_gateway_method.api_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-
-  uri = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${local.api_lambda_arn}/invocations"
-}
-
-resource "aws_api_gateway_method" "api_method_put" {
-  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-  resource_id   = aws_api_gateway_resource.api_resource.id
-  http_method   = "PUT"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "api_integration_put" {
-  rest_api_id             = aws_api_gateway_rest_api.rest_api.id
-  resource_id             = aws_api_gateway_resource.api_resource.id
-  http_method             = aws_api_gateway_method.api_method_put.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-
-  uri = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${local.api_lambda_arn}/invocations"
-}
-
-resource "aws_lambda_permission" "allow_api_gateway" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = local.lambda_function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*"
-}
-
-resource "aws_api_gateway_deployment" "deployment" {
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
-  stage_name  = var.stage
-}
-
-resource "aws_api_gateway_stage" "stage" {
-  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-  stage_name    = var.stage
-  deployment_id = aws_api_gateway_deployment.deployment.id
-}
-
-
