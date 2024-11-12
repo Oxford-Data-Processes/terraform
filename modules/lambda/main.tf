f73737cedaf4dc710a53b4ca75cf7cc4eceae70f
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-11-12 11:08:53 +0000

    Add old versions cleanup

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -8,8 +8,20 @@ module "lambda_module" {
   environment_variables = {
     STAGE                       = var.stage
     AWS_ACCESS_KEY_ID_ADMIN     = var.aws_access_key_id_admin
-    AWS_SECRET_ACCESS_KEY_ADMIN = var.aws_secret_access_key_admin
+    AWS_SECRET_ACCESS_KEY_ADMIN = var.aws_secret_access_key_id_admin
   }
   timeout     = 900
   memory_size = 2048
+}
+
+resource "null_resource" "cleanup_old_versions" {
+  triggers = {
+    image_uri = module.lambda_module.image_uri
+  }
+  
+  provisioner "local-exec" {
+    command = <<EOT
+      aws lambda list-versions-by-function --function-name ${var.lambda_function_name} --query 'Versions[?Version!=`$LATEST`].Version' --output text | xargs -I {} aws lambda delete-function --function-name ${var.lambda_function_name} --qualifier {}
+    EOT
+  }
 }
