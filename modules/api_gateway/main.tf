186d8eb978567931533a4a412c6f82c61dbaeec5
Christopher Little <63648951+chrismlittle123@users.noreply.github.com>
2024-11-13 17:31:26 +0000

    Deploy api 9

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -40,7 +40,36 @@ resource "aws_api_gateway_integration" "api_integration" {
   http_method = aws_api_gateway_method.api_get_method.http_method
   resource_id = aws_api_gateway_resource.api_resource.id
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  integration_http_method = "POST"
   type        = "AWS_PROXY"
+
+  uri = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${local.api_lambda_arn}/invocations"
+}
+
+resource "aws_api_gateway_method" "api_post_method" {
+  authorization = "NONE"
+  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.api_resource.id
+  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+}
+
+resource "aws_api_gateway_integration" "api_integration" {
+  http_method = aws_api_gateway_method.api_post_method.http_method
+  resource_id = aws_api_gateway_resource.api_resource.id
+  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  integration_http_method = "POST"
+  type        = "AWS_PROXY"
+
+  uri = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${local.api_lambda_arn}/invocations"
+}
+
+resource "aws_lambda_permission" "allow_api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = local.lambda_function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_api_gateway_rest_api.rest_api.execution_arn}/*"
 }
 
 resource "aws_api_gateway_deployment" "deployment" {
